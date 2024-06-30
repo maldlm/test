2a9ede80c1eb0607f746ce39def26c39882ff659
maldlm <145152015+maldlm@users.noreply.github.com>
2024-06-30 18:00:34 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
-console.log('hee this is test dlaim 3');
+console.log('hee this is test dlaim 4');
+
+const intervalId = setInterval(() => {
+  console.log('run4...');
+  const xBtn = document.getElementById('review-order-confirm-button-bottom');
+  if (xBtn) {
+    xBtn.style.setProperty('background-color', 'blue', 'important');
+    clearInterval(intervalId);
+  }
+}, 100);
